5d3ee01da8893226659f17411f0c303a73dcc5c4
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-09-13 10:18:03 +0300

    first

---
+++ lib/cubits/state.dart
@@ -0,0 +1,6 @@
+
+abstract class WeatherState{}
+class WeatherStateInit extends WeatherState{}
+class WeatherLoading extends WeatherState{}
+class WeatherSuccess extends WeatherState{}
+class WeatherFailure extends WeatherState{}
